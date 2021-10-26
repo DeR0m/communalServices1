5a23f126dae72d69108aeb819942ff2cb6783e38
DeR0m <denisromantsov@mail.ru>
2021-10-27 02:04:58 +0300

    init project

---
+++ src/test/java/com/example/communalServices/CommunalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communalServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
